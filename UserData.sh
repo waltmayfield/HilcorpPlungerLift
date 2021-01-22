c99e996d02468ba332b4c9e9c93b3977e33be5d0
Walter Mayfield <wmayfield@hilcorp.com>
2021-01-22 11:42:44 -0600

    Entered test mode

--- UserData.sh
+++ UserData.sh
@@ -11,8 +11,12 @@ python2.7 gpumon.py
 
 #Not sure if these will be accessable to ec2-user if made by root
 mkdir /home/ubuntu/EBSPlungerFiles
+mkdir /home/ubuntu/EBSPlungerFiles/Models
+mkdir /home/ubuntu/EBSPlungerFiles/TFRecordFiles
+mkdir /home/ubuntu/EBSPlungerFiles/RecommenedSettings
 cd /home/ubuntu/EBSPlungerFiles
 git clone https://github.com/waltmayfield/HilcorpPlungerLift
+
 cd ../
 
 docker create --gpus all -it --name testContainer --mount type=bind,source=/home/ubuntu/EBSPlungerFiles,target=/EBSPlungerFiles tensorflow/tensorflow:latest-gpu
