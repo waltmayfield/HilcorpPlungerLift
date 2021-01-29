87faaf59be44d27ab41b97f4cab2e046960353df
Walter Mayfield <wmayfield@hilcorp.com>
2021-01-29 11:16:15 -0600

    Changed imputation to KNN

--- PrepTFRecord.py
+++ PrepTFRecord.py
@@ -4,6 +4,7 @@
 import os
 import boto3
 import pandas as pd; import numpy as np
+from sklearn.impute import KNNImputer
 import tensorflow as tf
 from datetime import datetime
 import tqdm
@@ -18,6 +19,11 @@ prefix = 'DataByAPI/'
 # os.system('aws s3 sync s3://hilcorp-l48operations-plunger-lift-main/DataByAPI/ ~/EBSPlungerFiles/DataByAPI/')
 
 #This is a new path which may be more efficient for training
+def replaceNanOrInf(X):
+    bMask = tf.math.logical_or(tf.math.is_nan(X),tf.math.is_inf(X))
+    return tf.where(bMask,-1.,X)
+
+#This is a new path which may be more efficient for training
 def csv_to_tensor(file_path):
     #First read the file into a string
     sCsv = tf.io.read_file(file_path)#, [tf.constant('-1')]*1)#.numpy().decode('utf-8')
@@ -28,17 +34,23 @@ def csv_to_tensor(file_path):
     colSplit = tf.strings.split(rowSplit, sep=b',', maxsplit=-1, name='SplitLines')
     #Remove the header. The last line will be empty b/c the previous ended with the new line character. Convert to tensor
     colSplit = colSplit[1:-1,4:].to_tensor()#This removes the column header and the empty last row
-    #Replace empty strings with -1
-    colSplit = tf.where(tf.equal(colSplit, b''), b'-1', colSplit)
+
+    # #Replace empty strings with -1
+    # colSplit = tf.where(tf.equal(colSplit, b''), b'-1', colSplit)
+    
     #Convert from string to float32
     outTensor = tf.strings.to_number(colSplit, out_type = tf.dtypes.float32, name = 'f32TensorCsv')
 
-    return outTensor
+    #Use KNN to impute missing data
+    imputer = KNNImputer(n_neighbors=2)
+    outTensor = tf.constant(imputer.fit_transform(outTensor))
+    
+    # #Replace missing values with imputed values
+    # outTensor = tfp.sts.MaskedTimeSeries(
+    #     time_series=outTensor,
+    #     is_missing=replaceNanOrInf(outTensor))
 
-#This is a new path which may be more efficient for training
-def replaceNanOrInf(X):
-    bMask = tf.math.logical_or(tf.math.is_nan(X),tf.math.is_inf(X))
-    return tf.where(bMask,-1.,X)
+    return outTensor
 
 def process_path(file_path):
 
@@ -141,7 +153,7 @@ def convert_ds_to_TFRecord(ds, name, directory):
                     }))
 
             writer.write(example.SerializeToString())
-            print(f'Wrote example # {num_elements}')
+            print(f'Wrote example # {num_elements}, shape: {X.shape}')
         filename = os.path.join(directory, f'{name}-{num_elements}-Records.tfrecords')
         os.rename(tempFileName,filename)
     return filename
