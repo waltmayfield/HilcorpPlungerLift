a6382bf8f8025bdf3144557e70698545b7893245
Walter Mayfield <wmayfield@hilcorp.com>
2021-01-29 12:48:03 -0600

    Reduced batch size to 8

--- TrainValueFunction.py
+++ TrainValueFunction.py
@@ -21,7 +21,7 @@ numGPUs = len(lGpus)
 ######################### Training Parameters ########################
 training_epochs = 100
 validation_split = 0.1
-batch_size = numGPUs*30
+batch_size = numGPUs*8
 num_parallel_calls = 8
 buffer_size = 8
 ######################################################################
