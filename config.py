7d335074ad27c9aef3fd5c91d5967ccfbd8e5218
Walter Mayfield <wmayfield@hilcorp.com>
2021-02-13 10:42:29 -0600

    Added config file

---
+++ config.py
@@ -0,0 +1,9 @@
+[profile my-sso-profile-production]
+credential_process = aws-sso-credential-process --profile my-sso-profile
+region = us-west-2
+output = json
+sso_start_url = https://hilcorp.awsapps.com/start#/
+sso_region = us-west-2
+sso_account_id = 446356438225
+sso_role_name = DataScientistProduction
+#sso_role_name = DeveloperUserAccess
