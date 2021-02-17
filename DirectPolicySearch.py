b583cebbc9634f382de8402499ba91ddda205e4c
Walter Mayfield <wmayfield@hilcorp.com>
2021-02-17 08:23:24 -0600

    Correct model

--- DirectPolicySearch.py
+++ DirectPolicySearch.py
@@ -36,7 +36,7 @@ homeDirectory = r'/EBSPlungerFiles/'
 bucket_name = 'hilcorp-l48operations-plunger-lift-main'
 
 #model_name = r'Checkpoints/2020-12-16_460k_Param_LSTM_Skip_resBlock_311Epoch.h5'
-model_name = r'Checkpoints/2021-01-29_469472-TrainableVars_LSTM_Skip_resBlock_Larger_MCFD_Leg.h5'
+model_name = r'2021-01-29_469472-TrainableVars_LSTM_Skip_resBlock_Larger_MCFD_Leg.h5'
 
 model_save_location = homeDirectory + r'Models/' + model_name
 
