5cd9a4dfba8e4d395ad6ca484dc7a3644dd0b49a
Walter Mayfield <wmayfield@hilcorp.com>
2021-03-25 13:51:35 -0500

    Better test function

--- test.py
+++ test.py
@@ -1,65 +1,27 @@
-import pyodbc 
+import requests
+import base64
+import json
 
-# import os, io
-# import datetime
-# import pytz
-# import boto3
-# import matplotlib.pyplot as plt, pandas as pd
-# # import fsspec, s3fs
+ENCODING = 'utf-8'
+filePath = r"C:\Users\wmayfield\Desktop\Turdus-migratorius-002.jpg"
+fileName = r"Turdus-migratorius-003-python.jpg"
 
-# # pd.set_option("display.precision", 0)
-# pd.set_option('display.max_rows', 500)
-# pd.set_option('display.max_columns', 500)
-# pd.set_option('display.width', 1000)
-# pd.options.display.float_format = '{:,.0f}'.format
+with open(filePath, "rb") as image_file:
+    encoded_string = base64.b64encode(image_file.read())
 
-# # sHistoryKey = r'LossCurves/2021-01-29-LossCurves.csv'
-# sHistoryKey = r'LossCurves/2020-12-16-LossCurves.csv'
+# print(len(encoded_string))
 
-# ##################### Authentication #########################################
-# ##This is where the session will look for the profile name
-# os.environ['AWS_CONFIG_FILE'] = os.path.abspath('./config')
-# # r"C:\Users\wmayfield\Documents\HilcorpPlungerLift\config"
-# #r'U:\Projects\ML Plunger Lift Optimizer\.aws\config'
+url = 'https://gv1yedhm7b.execute-api.us-west-2.amazonaws.com/prod/UploadToS3'
 
-# sProfile = 'my-sso-profile-production' #Production version
-# #print(os.system(f'aws sso login --profile {sProfile}')) ##Comment this out after the first run. Run again if get SSO error.
-# session = boto3.Session(profile_name=sProfile)
-# ################################################################################
+myobj = json.dumps({
+	'name': fileName,
+	'species': 'Flamingo',
+	'file': encoded_string.decode(ENCODING)
+	},
+	indent = 2)
 
-# bucket_name = 'hilcorp-l48operations-plunger-lift-main'
-# sHistoryURI = f's3://hilcorp-l48operations-plunger-lift-main/LossCurves/{sHistoryKey}'
+x = requests.post(url, data = myobj)
 
-# s3_client = session.client('s3')
+print(x.text)
 
-# #Download the object using boto3
-# # try:
-# # 	obj = s3_client.get_object(Bucket=bucket_name, Key=sHistoryKey)
-# # except:
-# # 	print(os.system(f'aws sso login --profile {sProfile}')) #SSO Log on
-# # 	obj = s3_client.get_object(Bucket=bucket_name, Key=sHistoryKey)
-
-# print(os.system(f'aws s3 ls --profile {sProfile}'))
-# print('hello world')
-
-# print(os.system(f'aws sso login --profile {sProfile}'))
-# print(os.system('aws s3api list-buckets --query "Buckets[].Name"'))
-# print(os.system(f'aws sso login --profile {sProfile}'))
-
-# try:
-#     lApisInBucket = [o.key[len(prefix):-4] for o in allObjects if o.key[0:len(prefix)] == prefix]
-# except:
-#     print(os.system(f'aws sso login --profile {sProfile}'))
-#     lApisInBucket = [o.key[len(prefix):-4] for o in allObjects if o.key[0:len(prefix)] == prefix]
-
-
-
-# engine = psycopg2.connect(
-#     database="postgres",
-#     user="my_user_name",
-#     password="abc123def345",
-#     host="plot-db-production.cluster-ro-c1x26qtu2wyp.us-west-2.rds.amazonaws.com",
-#     port='5432'
-# )
-
-# print('done')
+### data:image/jpeg;base64,
