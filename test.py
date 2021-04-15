65e0be059dc66c679f7799b0fa284487a901add7
Walter Mayfield <wmayfield@hilcorp.com>
2021-04-15 09:55:57 -0500

    trying new cnxn string

--- test.py
+++ test.py
@@ -8,8 +8,8 @@ database = 'SJDW_BI'
 print("Attempting to initiate connection to server: " + server +", database: " + database)
 
 #This makes the connection with the server. If there is an error here the connection failed.
-# cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER='+server+';DATABASE='+database+';Trusted_Connection=yes;')
-cnxn = pyodbc.connect('DRIVER={ODBC Driver 13 for SQL Server};SERVER='+server+';DATABASE='+database+';uid=home;pwd=1234;')
+cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER='+server+';DATABASE='+database+';Trusted_Connection=yes;')
+# cnxn = pyodbc.connect('DRIVER={ODBC Driver 13 for SQL Server};SERVER='+server+';DATABASE='+database+';uid=home;pwd=1234;')
 
 sql = """
 	SELECT TOP 10 A.AssetCode, A.AssetName
