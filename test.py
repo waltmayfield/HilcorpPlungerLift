cf2547d8e0331d6fafb144fb486b775e287fe0d0
Walter Mayfield <wmayfield@hilcorp.com>
2021-04-15 09:40:31 -0500

    Simplifed and improved prints in test

--- test.py
+++ test.py
@@ -1,10 +1,11 @@
-# import cx_Oracle
 import pandas as pd
 import pyodbc
 
 #Server and database info
 server = 'frmsql03'
 database = 'SJDW_BI' 
+
+print(f"Attempting to initiate connection to server: {server}, database:{database}")
 
 #This makes the connection with the server. If there is an error here the connection failed.
 # cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER='+server+';DATABASE='+database+';Trusted_Connection=yes;')
@@ -20,40 +21,3 @@ print(sql)
 wells_df = pd.read_sql(sql,cnxn)
 
 print(wells_df.head())
-
-
-# cx_Oracle.init_oracle_client(lib_dir=r'C:\Users\wmayfield\instantclient_19_8')
-# dsn = """(DESCRIPTION =
-#     (ADDRESS = (PROTOCOL = TCP)(HOST = aztplotdb.hilcorp.com)(PORT = 1521))
-#     (CONNECT_DATA =
-#       (SERVER = DEDICATED)
-#       (SERVICE_NAME = oplosjt1.world)
-#     )
-#   )
-#     """
-
-# connection = cx_Oracle.connect("wmayfield", os.environ["ORACLEPSWD"], dsn, encoding="UTF-8")
-
-# def loadSQL(sql):#Load Oracle SQL Query
-#     cursor = connection.cursor()
-#     cursor.execute(sql)
-
-#     column_names = list()
-#     for i in cursor.description:
-#         column_names.append(i[0])
-
-#     return pd.DataFrame(cursor.fetchall(), columns = column_names)
-
-
-
-
-# #Find all wells in the PLOT Database
-# sql = """
-# SELECT 
-#     --Well Identity, DTTM, Cycle_Index
-#     DISTINCT D.WELLIDA AS UWI
-# FROM
-#     PLOT.WELL_LIST_WITH_DATA D
-# """
-
-# seriesUWIs = loadSQL(sql).UWI
