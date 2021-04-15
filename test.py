7b3f6596b06bef42153ef301efad60e25dde40de
Walter Mayfield <wmayfield@hilcorp.com>
2021-04-15 09:57:45 -0500

    Fixed drivers

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ database = 'SJDW_BI'
 print("Attempting to initiate connection to server: " + server +", database: " + database)
 
 #This makes the connection with the server. If there is an error here the connection failed.
-cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER='+server+';DATABASE='+database+';Trusted_Connection=yes;')
+cnxn = pyodbc.connect('DRIVER={ODBC Driver 13 for SQL Server};SERVER='+server+';DATABASE='+database+';Trusted_Connection=yes;')
 # cnxn = pyodbc.connect('DRIVER={ODBC Driver 13 for SQL Server};SERVER='+server+';DATABASE='+database+';uid=home;pwd=1234;')
 
 sql = """
